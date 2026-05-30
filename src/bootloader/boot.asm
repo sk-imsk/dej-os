bd00652d67da3e2aaa76c9c1cc2e9ccd2de84ea9
evan <evan@aleran.com.au>
2026-05-30 14:59:55 +1000

    I love fat (i got fat)

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -3,6 +3,41 @@ bits 16
 
 %define ENDL 0x0D, 0x0A
 
+
+
+;
+; fat 12 stuff lol
+;
+jmp short start
+nop
+
+
+bdb_oem: 			db 'MSWIN4.1'
+bdb_bytes_per_sector: 		dw 512
+bdb_sectors_per_sector: 	db 1
+bdb_reserved_sectors: 		dw 1
+bdb_fat_count: 			db 2
+bdb_dir_entrys_count:		dw 0E0H		
+bdb_total_sectors:		dw 2880
+bdb_media_descriptor_type:	db 0F0h	
+bdb_sectors_per_fat:		dw 9
+bdb_sectors_per_track:		dw 18
+bdb_heads:			dw 2
+bdb_hidden_sectors:		dd 0
+bdb_large_sector_count:		dd 0
+
+
+; extended boot record
+
+ebr_drive_number:		db 0
+				db 0 					; reserved
+ebr_signature:			db 29h
+ebr_volume_id:			db 12h, 34h, 56h, 76h 			; baller serial number (no one cares)
+ebr_volume_label:		db 'DEJ OS    '			; 11 byets  (i love spaces)
+ebr_system_id:			db 'FAT_12  ' 
+
+
+
 start: 
 	jmp main
 
