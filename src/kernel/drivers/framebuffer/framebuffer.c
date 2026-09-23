9bac8ce442599f4f0c9d33315bc19cb4ad49aca8
evan <pikachududee@gmail.com>
2026-09-23 17:43:07 +1000

    added screen info if feel like ig

--- src/kernel/drivers/framebuffer/framebuffer.c
+++ src/kernel/drivers/framebuffer/framebuffer.c
@@ -7,6 +7,7 @@
 #include <dej/limine.h>
 #include <dej/kernel.h>
 #include <dej/string.h>
+#include "framebuffer.h"
 
 extern struct limine_framebuffer *framebuffer;
 extern volatile uint32_t *fb_ptr;                       // get main to do our work lol
@@ -31,4 +32,15 @@ void clearscreen(void){
 
 struct limine_framebuffer * RequestFrameBuffer(uint8_t num){
     return framebuffer_request.response->framebuffers[num];
+}
+
+
+screen_t GetScreenInfo(uint8_t num){
+    screen_t ret;
+
+    ret.height = framebuffer[num].height;
+    ret.pitch = framebuffer[num].pitch;
+    ret.width = framebuffer[num].width;
+
+    return ret;
 }
--- src/kernel/drivers/framebuffer/framebuffer.h
+++ src/kernel/drivers/framebuffer/framebuffer.h
@@ -1,4 +1,12 @@
 #include <stdint.h>
+
+typedef struct {
+    uint64_t width, height;
+    uint64_t pitch;
+}screen_t;
+
+
 void putpixel(uint64_t x, uint64_t y, uint32_t colour);
 void clearscreen(void);
 struct limine_framebuffer * RequestFrameBuffer(uint8_t num);
+screen_t GetScreenInfo(uint8_t num);
