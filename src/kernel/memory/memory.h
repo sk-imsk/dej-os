2d46189d11d8d1efab97c0b2e4e84fed3a00b659
evan <pikachududee@gmail.com>
2026-09-24 20:19:15 +1000

    restore memory from the grave

---
+++ src/kernel/memory/memory.h
@@ -0,0 +1,21 @@
+#include <limine.h>
+#include <dej/kernel.h>
+
+
+typedef uint64_t page_entry_t;
+typedef uint64_t raw_page;
+
+#define PAGE_PRESENT  (1ULL << 0)
+#define PAGE_WRITE    (1ULL << 1)
+#define PAGE_USER     (1ULL << 2)
+#define PAGE_NX       (1ULL << 63)
+
+#define PAGE_SIZE KiB(4)
+
+
+
+int memory_init(struct limine_memmap_response * memmap, struct limine_hhdm_response * hhdm);
+void * KGetPage(void);
+void retpage(void * ptr);
+bool vm_map(uint64_t * pm14, uint64_t virt, uint64_t phys, uint64_t flags);
+bool vm_unmap(uint64_t * pm14, uint64_t phys);
